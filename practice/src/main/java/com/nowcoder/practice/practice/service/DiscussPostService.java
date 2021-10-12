66f88522e59e30bb43f3597a858b7470e1d8defc
zhenghang <380110237@qq.com>
2021-10-12 21:04:01 +0800

    显示登录状态，修改密码

--- practice/src/main/java/com/nowcoder/practice/practice/service/DiscussPostService.java
+++ practice/src/main/java/com/nowcoder/practice/practice/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.nowcoder.practice.practice.service;
 
 import com.nowcoder.practice.practice.dao.DiscussPostMapper;
 import com.nowcoder.practice.practice.entity.DiscussPost;
+import com.nowcoder.practice.practice.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -11,10 +13,22 @@ import java.util.List;
 public class DiscussPostService {
     @Autowired
     DiscussPostMapper discussPostMapper;
+    @Autowired
+    SensitiveFilter sensitiveFilter;
     public int findDiscussPostsRows(int userId){
         return discussPostMapper.selectDiscussPostRows(userId);
     }
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode){
         return discussPostMapper.selectDiscussPosts(userId,offset,limit,orderMode);
+    }
+    public int insertDiscussPosts(DiscussPost discussPost){
+        if(discussPost == null){
+            throw new IllegalArgumentException(("参数不能为空!"));
+        }
+//        discussPost.setTitle(HtmlUtils.htmlEscape(discussPost.getTitle()));
+//        discussPost.setContent(HtmlUtils.htmlEscape(discussPost.getContent()));
+        discussPost.setTitle(sensitiveFilter.filter(discussPost.getTitle()));
+        discussPost.setContent(sensitiveFilter.filter(discussPost.getContent()));
+        return discussPostMapper.insertDiscussPosts(discussPost);
     }
 }
--- practice/src/test/java/com/nowcoder/practice/practice/PracticeApplicationTests.java
+++ practice/src/test/java/com/nowcoder/practice/practice/PracticeApplicationTests.java
@@ -4,6 +4,8 @@ import com.nowcoder.practice.practice.dao.DiscussPostMapper;
 import com.nowcoder.practice.practice.dao.UserMapper;
 import com.nowcoder.practice.practice.util.MailClient;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.thymeleaf.TemplateEngine;
@@ -17,6 +19,7 @@ class PracticeApplicationTests {
 	DiscussPostMapper discussPostMapper;
     @Autowired
 	TemplateEngine templateEngine;
+    private static final Logger logger = LoggerFactory.getLogger(PracticeApplicationTests.class);
 	@Test
 	public void testFindUserById(){
 		System.out.print(discussPostMapper.selectDiscussPost(109));
@@ -38,5 +41,12 @@ class PracticeApplicationTests {
 	public void deleteUser(){
 		userMapper.deleteUser(183);
 	}
-
+	@Test
+	public void testLogger(){
+		try {
+			int a = 3/0;
+		} catch (Exception e) {
+			logger.debug("不能除以零!");
+		}
+	}
 }
